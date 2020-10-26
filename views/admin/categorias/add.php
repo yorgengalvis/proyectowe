efa8136123763bd79bb1bdf4ef093f49456d2910
Yorgen Galvis <yorgengalvis2000@gmail.com>
2020-10-26 13:32:59 -0500

    Modificaciones nuevas

---
+++ views/admin/categorias/add.php
@@ -0,0 +1,21 @@
+<div class="container-fluid">
+	<nav aria-label="breadcrumb">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item"><a href="index">Inicio</a></li>
+			<li class="breadcrumb-item "><a href="categorias">Categorias</a></li>
+			<li class="breadcrumb-item active"><a href="marcas">Agregar</a></li>
+		</ol>
+	</nav>
+</div>
+<div class="container-fluid">
+	<div class="card">
+		<div  class="card card-header title-p">
+			<center>Nueva Categoria</center>
+		</div>
+		<div class="card card-body">
+			<form action="<?=BASE_URL?>producto/edit/" method="POST">
+				dads
+			</form>
+		</div>
+	</div>
+</div>
--- views/admin/productos/edit.php
+++ views/admin/productos/edit.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
 	<nav aria-label="breadcrumb">
 		<ol class="breadcrumb">
-			<li class="breadcrumb-item"><a href="index">Inicio</a></li>
+			<li class="breadcrumb-item"><a href="<?=BASE_URL?>admin/index">Inicio</a></li>
 			<li class="breadcrumb-item "><a href="<?=BASE_URL?>admin/productos">Productos</a></li>
 			<li class="breadcrumb-item active"><a href='<?=BASE_URL?>admin/edit_product/<?=$producto[0]->id?>'>Editar</a></li>
 		</ol>
@@ -15,7 +15,36 @@
 		</div>
 		<div class="card card-body">
 			<form action="<?=BASE_URL?>producto/edit/" method="POST">
-				dads
+				<div class="row">
+					<div class="col-sm-2"><div class="form-group">
+    <label for="referencia">Referencia</label>
+    <input type="email" class="form-control" name="referencia" id="referencia" 
+          placeholder="SD4-55" value="<?=$producto[0]->referencia?>">
+  </div></div>
+					<div class="col-sm-5"><div class="form-group">
+    <label for="nombre">Nombre</label>
+    <input type="text" style="text-align: center;" class="form-control" name="nombre" id="nombre" 
+            placeholder="Ingrese nombre" value="<?=$producto[0]->nombre?>">
+  </div></div>
+					<div class="col-sm-4"><div class="form-group">
+    <label for="valor">Valor $</label>
+    <input type="number" style="text-align: center;" class="form-control" name="valor" id="valor" 
+           placeholder="<?=number_format($producto[0]->valor,2, ',', '.')?>">
+  </div></div>
+				</div>
+				<div class="col-lg-10">
+					<div class="row">
+						<div class="col-sm">
+						<label for="descripcioncorta">Descripcion corta</label>
+						<input type="text" style="text-align: center;" class="form-control" name="descripcioncorta" id="descripcioncorta" placeholder="valor">
+						
+						</div>
+						<div class="col-sm">
+							<label for="descripcioncorta">Detalle</label>
+
+						</div>
+					</div>
+				</div>
 			</form>
 		</div>
 	</div>
